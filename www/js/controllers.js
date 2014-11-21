e91807a9482568523e4a914e5c72c0cd2c3186cb
Debi xie <debi_xie@mtelnet.com>
2014-11-21 15:36:26 +0800

    change  some function of cart page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,77 @@ angular.module('starter.controllers', [])
 
 .controller('homepageCtrl', function($scope) {})
 
-.controller('sortpageCtrl', function($scope, Friends) {})
+.controller('sortpageCtrl', function($scope) {})
 
-.controller('askpageCtrl01', function($scope, $stateParams, Friends) {
-	$scope.friend = Friends.get($stateParams.friendId);
-})
+.controller('askpageCtrl01', function($scope, $stateParams) {})
 
 .controller('cartpageCtrl', function($scope) {})
-// 我的购物车 页面控制器
-.controller('mycartCtrl',function($scope,getHttp){
-
-         getHttp.get_data(carturl).then(function(_data) {
-         alert(_data[0].detail);
-        });	
-
-
-
-})
-// 我的购物车 编辑页面 控制器
-.controller('cartchangeCtrl',function($scope){
+	// 我的购物车 页面控制器
+	.controller('mycartCtrl', function($scope, getHttp) {
+		$scope.cartdatas = '';
+		getHttp.get_data(carturl).then(function(_data) {
+			$scope.cartdatas = _data;
+		});
 
 
-})
+
+	})
+	// 我的购物车 编辑页面 控制器
+	.controller('cartchangeCtrl', function($scope, getHttp) {
+		$scope.cartdatas = [];
+		getHttp.get_data(carturl).then(function(_data) {
+			$scope.cartdatas = _data;
+			console.info($scope.cartdatas);
+		});
+		$scope.value1 = "";
+		// 减少某件商品的数量
+		$scope.reduceNum = function(_index) {
+
+				if ($scope.cartdatas[_index].number == 1) {
+					$scope.cartdatas[_index].number = 1;
+				} else {
+					$scope.cartdatas[_index].number -= 1;
+					getHttp.post_data(carturl, $scope.cartdatas[_index]).then(function(_data) {
+						alert(_data);
+					}, function() {
+						alert("error");
+					})
+				}
+			}
+			// 增加某件商品的数量
+		$scope.addNum = function(_index) {
+				$scope.cartdatas[_index].number += 1;
+			}
+			// 处理当选择checkbox时，插入selectPermissions 数组中
+			// 当点击checkbox 时 检查selectPermissions 数组是否已经存在了该 id
+			// 如果存在 返回-1 ；转换checkbox 的状态 即把该ID 删除id
+			// 否则 插入selectPermissions 数组
+		$scope.selectedPermissions = [];
+
+		$scope.toggleSelection = function toggleSelection(permission) {
+			var index = $scope.selectedPermissions.indexOfObjectWithProperty('id', permission.id);
+			if (index > -1) {
+				// Is currently selected, so remove it
+				$scope.selectedPermissions.splice(index, 1);
+				console.log(index);
+			} else {
+				// Is currently unselected, so add it
+				$scope.selectedPermissions.push(permission);
+				console.log(permission);
+			}
+		};
+		// 删除购物车的商品函数
+		$scope.deleteCartData = function() {
+			for (var i = 0, len = $scope.selectedPermissions.length; i < len; i++) {
+			for(var j=0,lens=$scope.cartdatas.length;j<lens;j++){
+				if($scope.cartdatas[j].id==$scope.selectedPermissions[i].id){
+					$scope.cartdatas.splice(j,1);
+				}
+			}
+			}
+		}
+
+	})
 	.controller('personCtrl', function($scope) {})
 	.controller('productpageCtrl', function($scope) {
 		$scope.showflage = false;
@@ -47,24 +96,41 @@ angular.module('starter.controllers', [])
 					console.log("your name is invalid !!!");
 				} else if (!_Pisvalid) {
 					console.log("your password is invalid !!!");
-				}else{
+				} else {
 					console.log("your form is ok ");
 				}
 			}
 		}
 
 	})
-	.controller('askpageCtrl',function($scope,httpCard){
-    //通过id获取银行卡
-    $scope.card_1 = httpCard.getById(1);
-    $scope.card_2 = httpCard.getById(2);
-    $scope.card_3 = httpCard.getById(3);
-    //获取所有的银行卡
-    $scope.cards = httpCard.query();
-    //更新id为3的银行卡
-    $scope.updataCard = function(){httpCard.save({id:3,name:"工商银行"}).then(function(data){$scope.card_3 = data['name']})};
-    //添加id为4的银行卡
-    $scope.addCard = function(){httpCard.save({name:"浦发银行"}).then(function(data){$scope.card_4 = data['name']});};
-    //删除id为3的银行卡
-    $scope.delCard = function(){httpCard.del(3).then(function(data){$scope.card_3 = data});}
-})
+	.controller('askpageCtrl', function($scope, httpCard) {
+		//通过id获取银行卡
+		$scope.card_1 = httpCard.getById(1);
+		$scope.card_2 = httpCard.getById(2);
+		$scope.card_3 = httpCard.getById(3);
+		//获取所有的银行卡
+		$scope.cards = httpCard.query();
+		//更新id为3的银行卡
+		$scope.updataCard = function() {
+			httpCard.save({
+				id: 3,
+				name: "工商银行"
+			}).then(function(data) {
+				$scope.card_3 = data['name']
+			})
+		};
+		//添加id为4的银行卡
+		$scope.addCard = function() {
+			httpCard.save({
+				name: "浦发银行"
+			}).then(function(data) {
+				$scope.card_4 = data['name']
+			});
+		};
+		//删除id为3的银行卡
+		$scope.delCard = function() {
+			httpCard.del(3).then(function(data) {
+				$scope.card_3 = data
+			});
+		}
+	})
--- www/js/services.js
+++ www/js/services.js
@@ -16,6 +16,20 @@ angular.module('starter.services', ['ngResource'])
           deferred.reject();
         });
         return deferred.promise;
+      },
+      post_data: function(_url, _data) {
+        var deferred = $q.defer();
+        $http({
+          method: 'post',
+          url: _url,
+          data: _data
+        }).success(function(data) {
+          deferred.resolve(data);
+        }).error(function() {
+          /* Act on the event */
+          deferred.reject();
+        });
+        return deferred.promise;
       }
     }
   })
@@ -26,13 +40,13 @@ angular.module('starter.services', ['ngResource'])
   // CartProduct.removeProduciton 删除购物车的某个产品
   .factory('CartProduct', function(getHttp, $q) {
     var mycartPros;
-     var deferred = $q.defer();
-        getHttp.get_data(carturl).then(function(_data) {
-          deferred.resolve(_data);
-        }, function() {
-          deferred.reject();
-        });
-        mycartPros=deferred.promise;
+    var deferred = $q.defer();
+    getHttp.get_data(carturl).then(function(_data) {
+      deferred.resolve(_data);
+    }, function() {
+      deferred.reject();
+    });
+    mycartPros = deferred.promise;
     return {
       allProduction: function() {
         return mycartPros;
@@ -48,64 +62,99 @@ angular.module('starter.services', ['ngResource'])
       }
     }
   })
-.factory('httpCard',function($http,$q){
+  .factory('httpCard', function($http, $q) {
     var baseUrl = 'http://192.168.0.114:3000/card/user/123';
     return {
-        //获取用户123的指定id的卡
-        getById:function(id){
-            var defer = $q.defer();
-            $http({
-                method:'get',
-                url:baseUrl+'/'+id
-            }).success(function(data){
-                defer.resolve(data);
-            }).error(function(data){
-                defer.reject(data);
-            });
-            return defer.promise
-        },
-        //获取用户123的所有卡
-        query:function(){
-            var defer = $q.defer();
-            $http({
-                method:'get',
-                url:baseUrl
-            }).success(function(data){
-                defer.resolve(data);
-            }).error(function(data){
-                defer.reject(data);
-            });
-            return defer.promise
-        },
-        //保存一张卡,如果原来就有id,那就是更新这张卡,如果原来没有这张卡,那就是新建这张卡,
-        //新建路径就是baseUrl,更新路径就是baseUrl+id
-        save:function(card){
-            var defer = $q.defer();
-            var url = card.id ? baseUrl+'/'+card.id : baseUrl;
-            $http({
-                method:'post',
-                url:url,
-                data:card
-            }).success(function(data){
-                defer.resolve(data);
-            }).error(function(data){
-                defer.reject(data);
-            });
-            return defer.promise
-        },
-        //删除用户123的指定id的卡
-        del:function(id){
-            var defer = $q.defer();
-            $http({
-                method:'delete',
-                url:baseUrl+'/'+id
-            }).success(function(data){
-                defer.resolve(data)
-            }).error(function(data){
-                defer.reject(data)
-            });
-            return defer.promise
-        }
+      //获取用户123的指定id的卡
+      getById: function(id) {
+        var defer = $q.defer();
+        $http({
+          method: 'get',
+          url: baseUrl + '/' + id
+        }).success(function(data) {
+          defer.resolve(data);
+        }).error(function(data) {
+          defer.reject(data);
+        });
+        return defer.promise
+      },
+      //获取用户123的所有卡
+      query: function() {
+        var defer = $q.defer();
+        $http({
+          method: 'get',
+          url: baseUrl
+        }).success(function(data) {
+          defer.resolve(data);
+        }).error(function(data) {
+          defer.reject(data);
+        });
+        return defer.promise
+      },
+      //保存一张卡,如果原来就有id,那就是更新这张卡,如果原来没有这张卡,那就是新建这张卡,
+      //新建路径就是baseUrl,更新路径就是baseUrl+id
+      save: function(card) {
+        var defer = $q.defer();
+        var url = card.id ? baseUrl + '/' + card.id : baseUrl;
+        $http({
+          method: 'post',
+          url: url,
+          data: card
+        }).success(function(data) {
+          defer.resolve(data);
+        }).error(function(data) {
+          defer.reject(data);
+        });
+        return defer.promise
+      },
+      //删除用户123的指定id的卡
+      del: function(id) {
+        var defer = $q.defer();
+        $http({
+          method: 'delete',
+          url: baseUrl + '/' + id
+        }).success(function(data) {
+          defer.resolve(data)
+        }).error(function(data) {
+          defer.reject(data)
+        });
+        return defer.promise
+      }
     }
-})
+  })
+/**
+ * Searches an array for an object with a specified property.
+ * 
+ * Example usage:
+ * <code>var index = myArray.indexOfObjectWithProperty('id', 123);</code>
+ * 
+ * @param propertyName the name of the property to inspect
+ * @param propertyValue the value to match
+ * @returns the (zero-based) index at which the object was found, or -1 if no
+ * such object was found
+ */
+Array.prototype.indexOfObjectWithProperty = function(propertyName, propertyValue)
+{
+  for (var i = 0, len = this.length; i < len; i++)
+  {
+    if (this[i][propertyName] === propertyValue) return i;
+  }
 
+  return -1;
+};
+
+
+/**
+ * Test if an array of objects contains an object with a specified property.
+ * 
+ * Example usage:
+ * <code>var isPresent = myArray.containsObjectWithProperty('id', 123);</code>
+ * 
+ * @param propertyName the name of the property to inspect
+ * @param propertyValue the value to match
+ * @returns true if an object was found, false otherwise
+ */
+Array.prototype.containsObjectWithProperty = function(propertyName, propertyValue)
+{
+  return this.indexOfObjectWithProperty(propertyName, propertyValue) != -1;
+};
